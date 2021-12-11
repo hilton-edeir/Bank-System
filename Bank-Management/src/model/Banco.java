32b94ad9b6f36285b6b93694cbc93469e5656873
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-11 06:37:26 -0100

    update

--- Bank-Management/src/model/Banco.java
+++ Bank-Management/src/model/Banco.java
@@ -1,6 +1,5 @@
 package model;
-
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class Banco {
 
@@ -11,10 +10,28 @@ public class Banco {
         JOptionPane.showMessageDialog(null,"Seja bem-vindo ao Bank System",
                 "Bank System", JOptionPane.PLAIN_MESSAGE,null);
 
-        int opcao = JOptionPane.showOptionDialog(null, null,"Operações",
+        int opcao_escolhida = JOptionPane.showOptionDialog(null, null,"Operações",
                     JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]);
 
 
+        if(opcao_escolhida == 0){
+            System.out.println("Funcionalidade por implementar");
+        }
+        else if(opcao_escolhida == 1) {
 
+            String[] contas = {"Conta Correnta", "Conta Poupança", "Voltar"};
+            int conta_escolhida = JOptionPane.showOptionDialog(null, null,"Tipo de conta",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, contas, contas[0]);
+
+            if(conta_escolhida == 0) {
+                System.out.println("Conta corrente");
+            }
+            else if(conta_escolhida == 1){
+                System.out.println("Conta poupança");
+            }
+            else{
+                System.out.println("Voltar");
+            }
+        }
     }
 }
