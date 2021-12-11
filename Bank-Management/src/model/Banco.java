a7c33a850260ad85c7ba292d5859d7c95e821819
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-11 06:29:28 -0100

    update

--- Bank-Management/src/model/Banco.java
+++ Bank-Management/src/model/Banco.java
@@ -1,8 +1,20 @@
 package model;
 
+import javax.swing.*;
+
 public class Banco {
 
     public static void main(String[] args) {
+
+        String[] opcoes = {"Acessar a minha conta", "Abrir uma conta"};
+
+        JOptionPane.showMessageDialog(null,"Seja bem-vindo ao Bank System",
+                "Bank System", JOptionPane.PLAIN_MESSAGE,null);
+
+        int opcao = JOptionPane.showOptionDialog(null, null,"Operações",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]);
+
+
 
     }
 }
