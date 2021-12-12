90ffd4e757bc5dc060f429f778e130747fb5b5ea
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-12 12:46:33 -0100

    update

--- Bank-Management/src/model/Banco.java
+++ Bank-Management/src/model/Banco.java
@@ -8,13 +8,13 @@ public class Banco {
         Scanner ler = new Scanner(System.in);
 
         System.out.println("------- Bem-vindo ao Bank system -------\n");
-        System.out.println("1 - Acessar a minha conta\n2 - Abrir uma conta\n3 - Sair\n");
+        System.out.println("1 - Acessar conta\n2 - Abrir conta\n3 - Sair\n");
         System.out.print("Opção: ");
         int opcao_escolhida = ler.nextInt();
 
         switch(opcao_escolhida) {
             case 1 -> {
-                System.out.println("----------- Bank system -----------\n");
+                System.out.println("----------- Acessar conta -----------\n");
 
                 System.out.print("Número da conta: ");
                 long numero_conta = ler.nextLong();
@@ -26,20 +26,25 @@ public class Banco {
             }
 
             case 2 -> {
-                System.out.println("------- Bank system -------\n");
+                System.out.println("----------- Abrir conta -----------\n");
                 System.out.println("1 - Conta corrente\n2 - Conta poupança\n3 - Voltar");
                 System.out.print("\nOpção: ");
                 int tipo_conta = ler.nextInt();
 
                 switch (tipo_conta) {
-                    case 1 -> System.out.println("Por implementar");
+                    case 1 -> {
+                        System.out.println("Por implementar");
+                        String cni = ler.nextLine();
+                        Cliente cliente = new Cliente();
+                        //ContaCorrente conta_corrente= new ContaCorrente();
+                    }
 
-                    //ContaCorrente conta_corrente= new ContaCorrente();
                     case 2 -> {
                         System.out.println("w");
                         System.out.println("Por implementar");
+                        //ContaPoupanca conta_poupanca = new ContaPoupanca();
                     }
-                    //ContaPoupanca conta_poupanca = new ContaPoupanca();
+
                     case 3 -> {
                         System.out.println("f");
                         System.out.println("Por implementar");
