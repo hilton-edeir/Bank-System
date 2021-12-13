dfce5d4db0f1bb8f57f61e737acfca04893d2ac1
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-13 22:45:29 -0100

    update

--- Bank-Management/src/model/Banco.java
+++ Bank-Management/src/model/Banco.java
@@ -152,8 +152,6 @@ public class Banco {
                             System.out.println("Conta: " + conta_poupanca.getNumero());
                             System.out.println("Status: " + conta_poupanca.isStatus_ativo());
                             System.out.println("Data Criação: " + conta_poupanca.getData_criacao());
-                            System.out.println("Saldo: " + conta_poupanca.getSaldo() + " ECV");
-                            System.out.println("Palavra-passe: " + conta_poupanca.getPalavra_passe());
                         }
 
                         case 3 -> {
