5e3c548acb55cdba323ba2206ee82c4cad6b507d
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-12-14 20:41:05 -0100

    update

--- Bank-Management/src/model/Funcionario.java
+++ Bank-Management/src/model/Funcionario.java
@@ -56,4 +56,45 @@ public class Funcionario {
     public void setTelemovel(String telemovel) {
         this.telemovel = telemovel;
     }
+
+
+    public String getCNI() {
+        return CNI;
+    }
+
+    public void setCNI(String CNI) {
+        this.CNI = CNI;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(String data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+    public String getTelemovel() {
+        return telemovel;
+    }
+
+    public void setTelemovel(String telemovel) {
+        this.telemovel = telemovel;
+    }
 }
